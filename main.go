34f6dbc431faa92643541303f1e8995da00a288b
zhongtao <ztappleemail@163.com>
2025-07-13 16:53:45 +0800

    feat(main): 注释掉创建操作并添加读取操作
    - 注释掉了原有的创建操作
    - 添加了两种读取操作：1. 根据整型主键查找
      2.根据 code 字段值查找- 打印读取到的产品信息

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"GoStudy/gorm"
 	"GoStudy/gorm/model"
+	"fmt"
 )
 
 func main() {
 	db, _ := gorm.DBInit()
 
-	// Create
-	db.Create(&model.Product{Code: "D42", Price: 100})
+	//// Create
+	//db.Create(&model.Product{Code: "D42", Price: 100})
 
+	// Read
+	var product model.Product
+	db.First(&product, 1)                 // 根据整型主键查找
+	db.First(&product, "code = ?", "D42") // 查找 code 字段值为 D42 的记录
+	fmt.Println(product)
 }
