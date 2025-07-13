009465096bc9ce33e2bbd22aa4253d960bce45c2
zhongtao <ztappleemail@163.com>
2025-07-13 16:46:52 +0800

    feat(gorm): 添加 GORM相关代码并初始化数据库
    - 新增 db_init.go 文件，实现数据库连接和自动迁移功能
    - 新增 product.go 文件，定义 Product 模型
    - 在 main.go 中调用 DBInit 函数，启动数据库初始化
    - 更新 go.mod 文件，添加 GORM 和 MySQL驱动依赖

---
+++ gorm/db_init.go
@@ -0,0 +1,23 @@
+package gorm
+
+import (
+	"GoStudy/gorm/model"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func DBInit() {
+	dsn := "root:rootroot@tcp(47.122.87.56:3306)/gorm?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// 迁移 schema
+	err = db.AutoMigrate(
+		model.Product{},
+	)
+	if err != nil {
+		panic("failed to db.AutoMigrate")
+	}
+}
--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
-func main() {
+import "GoStudy/gorm"
 
+func main() {
+	gorm.DBInit()
 }
