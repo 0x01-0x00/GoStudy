e4b20c7278c72c9ac37281a4f3b2897dd479292a
zhongtao <ztappleemail@163.com>
2025-07-13 17:47:04 +0800

    feat(main): 添加产品数据的创建和检索功能
    - 在 main.go 中添加了创建产品记录的示例代码
    - 增加了读取单个和多个产品记录的示例代码
    - 添加了相应的中文注释，便于理解代码逻辑

--- main.go
+++ main.go
@@ -10,11 +10,21 @@ func main() {
 	db, _ := gorm.DBInit()
 
 	//// Create
+	//// 创建一条记录
 	//db.Create(&model.Product{Code: "D42", Price: 100})
+	fmt.Println("Create End!")
 
 	// Read
+	// 检索单个对象
 	var product model.Product
 	db.First(&product, 1)                 // 根据整型主键查找
 	db.First(&product, "code = ?", "D42") // 查找 code 字段值为 D42 的记录
 	fmt.Println(product)
+	fmt.Println("Read End!")
+
+	// 检索全部对象
+	var products []model.Product
+	db.Find(&products, "price = ?", 100) // 查找 price 字段值为 100 的记录
+	fmt.Println(products)
+	fmt.Println("Find End!")
 }
