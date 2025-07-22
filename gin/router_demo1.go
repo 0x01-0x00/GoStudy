c9f73a8d0261afe39da860117fd54e35d74970ef
zhongtao <ztappleemail@163.com>
2025-07-22 21:10:29 +0800

    feat(router_demo1): 增加 Apple 结构体并更新路由响应
    - 定义了一个新的 Apple 结构体，包含多个苹果产品字段
    - 在各个路由处理函数中添加了 Apple 结构体实例作为响应的一部分
    -演示了如何在 Gin框架中使用结构体进行 JSON 响应

--- gin/router_demo1.go
+++ gin/router_demo1.go
@@ -6,26 +6,47 @@ import (
 	"net/http"
 )
 
+type Apple struct {
+	Iphone  string
+	Mac     string
+	Watch   string
+	Airpods string
+	ipad    string // 小写私有字段
+	Ipod    string `json:"ipod"`
+}
+
 func RouterDemo1() {
+	apple := Apple{
+		Iphone:  "Iphone",
+		Mac:     "Mac",
+		Watch:   "Watch",
+		Airpods: "Airpods",
+		ipad:    "ipad",
+		Ipod:    "ipod",
+	}
 	// 定义路由
 	global.R.GET("/user", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "GET",
+			"message":     "GET",
+			"RouterDemo1": apple,
 		})
 	})
 	global.R.POST("/user", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "POST",
+			"message":     "POST",
+			"RouterDemo1": apple,
 		})
 	})
 	global.R.PUT("/user", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "PUT",
+			"message":     "PUT",
+			"RouterDemo1": apple,
 		})
 	})
 	global.R.DELETE("/user", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "DELETE",
+			"message":     "DELETE",
+			"RouterDemo1": apple,
 		})
 	})
 }
