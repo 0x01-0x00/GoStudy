67782916be65f6e37ea289d6d0c6aca4dcfaad11
zhongtao <ztappleemail@163.com>
2025-08-11 14:15:52 +0800

    refactor(gorm/model): 重命名 User 结构体为 UserTest
    - 将 User 结构体重命名为 UserTest，用于表示测试结构体
    - 更新了结构体字段的注释，以适应新的结构体名称
    - 此修改旨在提高代码的可读性和可维护性

--- gorm/model/user.go
+++ gorm/model/user.go
@@ -5,15 +5,16 @@ import (
 	"time"
 )
 
-type User struct {
-	ID           uint           // 主键的标准字段
-	Name         string         // 主键的标准字段
-	Email        *string        // 一个指向字符串的指针，允许为空值
-	Age          uint8          // 一个无符号的8位整数
-	Birthday     *time.Time     // 一个指向时间的指针。时间，可以为空
-	MemberNumber sql.NullString // 使用sql.NullString来处理可为空的字符串
-	ActivatedAt  sql.NullTime   // 对于可为空的时间字段，使用sql.NullTime
-	CreatedAt    time.Time      // 由GORM自动管理创建时间
-	UpdatedAt    time.Time      // 由GORM自动管理更新时间
-	ignored      string         // 未导出的字段将被忽略
+// UserTest 测试结构体
+type UserTest struct {
+	ID           uint           //主键的标准字段
+	Name         string         //常规字符串字段
+	Email        *string        //指向字符串的指针，允许空值
+	Age          uint8          //一个无符号的8位整数
+	Birthday     *time.Time     //指向时间的指针。时间，可以为空
+	MemberNumber sql.NullString //使用sql。NullString用于处理可为null的字符串
+	ActivatedAt  sql.NullTime   //使用sql。空时间字段的NullTime
+	CreatedAt    time.Time      //创建时间由GORM自动管理
+	UpdatedAt    time.Time      //由GORM自动管理更新时间
+	ignored      string         //未导出的字段将被忽略
 }
